e7469b2006b1dbff6b201b1721fbda7bf1ccc5cd
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2020-07-30 12:37:43 +0800

    get weights from dan's gdrive

--- weights/get_weights.sh
+++ weights/get_weights.sh
@@ -1,3 +1,12 @@
-wget -O yolov4.weights "https://www.dropbox.com/s/3qb94c9611ksac7/yolov4.weights?dl=0"
+# Checks if gdown is installed
+if ! type "gdown" > /dev/null; then
+    read -p "Install gdown (google drive downloader) with pip3 (y/n)? " yn
+    case $yn in
+        [Yy]* ) pip3 install gdown;;
+        * ) echo "Not installing gdown. Exiting.."; exit;;
+    esac
+fi
 
-wget -O yolov4.pth "https://www.dropbox.com/s/vhokcwud1akf4js/yolov4.pth?dl=0"
+gdown -O yolov4.weights https://drive.google.com/uc?id=1c3HsOmGkMTpxBwfhQS1-deOoN2We5TQY
+
+gdown -O yolov4.pth https://drive.google.com/uc?id=1lXcUVGI2Ns5hJlAq98pQMV43Ds3vzh96
