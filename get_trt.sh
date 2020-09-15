d785e572611c8d36fdee79a040f7f086c41d078e
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2020-09-15 17:48:15 +0800

    add trt download for cuda 10.2

--- get_trt.sh
+++ get_trt.sh
@@ -8,4 +8,7 @@ if ! type "gdown" > /dev/null; then
 fi
 
 # Downloads with gdown
+# cuda 10.0
 gdown -O nv-tensorrt-repo-ubuntu1804-cuda10.0-trt7.0.0.11-ga-20191216_1-1_amd64.deb https://drive.google.com/uc?id=10NT4GYOAOjrwdSGPJS6v6uyVtduW-Pa3
+# cuda 10.2
+# gdown -O nv-tensorrt-repo-ubuntu1804-cuda10.0-trt7.0.0.11-ga-20191216_1-1_amd64.deb https://drive.google.com/uc?id=12z2KrjQpRSzT_BFdjm4UPY8Sv8APqbdU
