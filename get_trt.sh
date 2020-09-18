3218b6b0ec9677f723dc65a6ea308bd748ff37f3
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2020-09-18 13:21:04 +0800

    oops typo

--- get_trt.sh
+++ get_trt.sh
@@ -11,4 +11,4 @@ fi
 # cuda 10.0
 gdown -O nv-tensorrt-repo-ubuntu1804-cuda10.0-trt7.0.0.11-ga-20191216_1-1_amd64.deb https://drive.google.com/uc?id=10NT4GYOAOjrwdSGPJS6v6uyVtduW-Pa3
 # cuda 10.2
-# gdown -O nv-tensorrt-repo-ubuntu1804-cuda10.0-trt7.0.0.11-ga-20191216_1-1_amd64.deb https://drive.google.com/uc?id=12z2KrjQpRSzT_BFdjm4UPY8Sv8APqbdU
+# gdown -O nv-tensorrt-repo-ubuntu1804-cuda10.2-trt7.0.0.11-ga-20191216_1-1_amd64.deb https://drive.google.com/uc?id=12z2KrjQpRSzT_BFdjm4UPY8Sv8APqbdU
